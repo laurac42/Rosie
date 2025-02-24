a2e85270c1527e77bd87b260355992972377b00b
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-02-24 21:41:45 +0000

    Updates

--- assets/status-tap-legacy-DuyVo-ie.js
+++ assets/status-tap-legacy-DuyVo-ie.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B8qEFUbL.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-BBHe4eZ9.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
