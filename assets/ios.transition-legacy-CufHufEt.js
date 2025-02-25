9bd9d881e511d46fbf0f99dbd9254ebd215c3218
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-02-25 16:44:51 +0000

    trying to get all files added again again

--- assets/ios.transition-legacy-CufHufEt.js
+++ assets/ios.transition-legacy-CufHufEt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-EkfSDTkf.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-AEVP_6Hl.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
