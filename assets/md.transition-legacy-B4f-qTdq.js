75210228058844a57f71331f6abe9c4af012650a
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-03 15:48:09 +0000

    removing hashtags so that all pages work but actually removing them all

--- assets/md.transition-legacy-B4f-qTdq.js
+++ assets/md.transition-legacy-B4f-qTdq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C_IlQTuD.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-w-ozoe2B.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
