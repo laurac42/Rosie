df8d811b51dbb10075bc36e33f5ce6cd7f481351
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-03 15:43:53 +0000

    removing hashtags so that all pages work

--- assets/ios.transition-legacy-Wb29TwQO.js
+++ assets/ios.transition-legacy-Wb29TwQO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CvTwBfg4.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-C_IlQTuD.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
