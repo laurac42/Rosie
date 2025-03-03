7175e8638234b267b4bac847de264ff2d7a91219
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-03 14:40:39 +0000

    Updates

--- assets/status-tap-legacy-C_fzzRgo.js
+++ assets/status-tap-legacy-C_fzzRgo.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DCycm165.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-5rVcau7I.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
