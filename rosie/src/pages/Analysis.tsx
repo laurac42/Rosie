28249cc55e4acc0851d8b1e43caee5e4f7b54ede
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-10 21:10:16 +0000

    everything works on analysis page

--- rosie/src/pages/Analysis.tsx
+++ rosie/src/pages/Analysis.tsx
@@ -1,7 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonButton, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/react';
 import { personCircle } from 'ionicons/icons';
 import React from 'react';
-import { BarChart } from '@mui/x-charts/BarChart';
+import { BarChart, PieChart } from '@mui/x-charts';
 import { axisClasses } from '@mui/x-charts/ChartsAxis';
 import { useState, useEffect } from 'react';
 import moment from 'moment';
@@ -15,6 +15,11 @@ const Analysis: React.FC = () => {
     const [cycleLengths, setCycleLengths] = useState<{ length: number, startDate: string }[]>([]);
     const [averagePeriodLength, setAveragePeriodLength] = useState<number>(0);
     const [averageCycleLength, setAverageCycleLength] = useState<number>(0);
+    const [crampDays, setCrampDays] = useState<string[]>([]);
+    const [noPainDays, setNoPainDays] = useState<string[]>([]);
+    const [backPainDays, setBackPainDays] = useState<string[]>([]);
+    const [headacheDays, setHeadacheDays] = useState<string[]>([]);
+
     const chartSetting = {
         yAxis: [
             {
@@ -23,21 +28,19 @@ const Analysis: React.FC = () => {
         ],
         width: 500,
         height: 300,
-       
+
     };
 
     // get average period length on first load
     useEffect(() => {
-        getAveragePeriodLength();
+        getStartAndEndDates();
+        getPainData();
     }, []);
 
     /**
      * Get all of the period data, calculate start and end dates, and calculate the average period length
      */
     function getAveragePeriodLength() {
-        //startDates.length = 0;
-        getStartAndEndDates();
-        getAverageCycleLength();
 
         // check number of start and end dates is equal
         if (startDates.length != endDates.length) {
@@ -49,57 +52,56 @@ const Analysis: React.FC = () => {
             const startMoment = moment(startDates[i]);
             const endMoment = moment(endDates[i]);
             const periodLength = endMoment.diff(startMoment, 'days') + 1;
-            periodLengths.push({length: periodLength, startDate: startDates[i]});
+            periodLengths.push({ length: periodLength, startDate: startDates[i] });
         }
 
         // calculate average
         var sum = 0;
-        for (let i=0; i<periodLengths.length; i++)
-        {
+        for (let i = 0; i < periodLengths.length; i++) {
             sum += periodLengths[i].length;
         }
         var averagePeriodLength = (sum / periodLengths.length) || 0;
 
         setAveragePeriodLength(Math.round(averagePeriodLength * 10) / 10);
-
-        console.log(averagePeriodLength);
-
     }
 
     /**
      * Get the average cycle length 
      * Calculate the difference in days between two start dates
      */
-    function getAverageCycleLength()
-    {   
+    function getAverageCycleLength() {
         cycleLengths.length = 0; // reset it each time it is calculated
         for (let i = 1; i < startDates.length; i++) {
-            const startMoment = moment(startDates[i-1]);
-            const endMoment = moment(endDates[i]);
+            const startMoment = moment(startDates[i - 1]);
+            const endMoment = moment(startDates[i]);
             const cycleLength = endMoment.diff(startMoment, 'days');
-            cycleLengths.push({length: cycleLength, startDate: startDates[i]});
+            //console.log(cycleLength);
+            cycleLengths.push({ length: cycleLength, startDate: startDates[i-1] });
         }
 
         // calculate average
         var sum = 0;
-        for (let i=0; i<cycleLengths.length; i++)
-        {
+        for (let i = 0; i < cycleLengths.length; i++) {
             sum += cycleLengths[i].length;
         }
         var averageCycleLength = (sum / cycleLengths.length) || 0;
 
         setAverageCycleLength(Math.round(averageCycleLength * 10) / 10);
-
-        console.log(averageCycleLength);
     }
 
+    /**
+     * Get the start and end dates of a period
+     */
     function getStartAndEndDates() {
+        // clear any old data first:
+        periods.length = 0;
+        startDates.length = 0;
         // first, load in all period data and make sure it is sorted by date
         var periodDates = new Map<string, string>(JSON.parse(localStorage.periodMap));
         periodDates.forEach((flow: string, date: string) => {
             if (!periods.includes(date)) { periods.push(date); }
         });
-        periods.sort((a, b) => (new Date(a).getTime() - new Date(b).getTime())); // from most recent
+        periods.sort((a, b) => (new Date(a).getTime() - new Date(b).getTime())); // from oldest
 
         // the first period is definitely a start date
         if (!startDates.includes(periods[0])) { startDates.push(periods[0]); }
@@ -119,8 +121,44 @@ const Analysis: React.FC = () => {
                 // periods i is an end date
                 if (!endDates.includes(periods[i])) { endDates.push(periods[i]); }
             }
-            //console.log("end dates: ",endDates);
         }
+
+        // call functions to do calculations based on start and end dates
+        getAverageCycleLength();
+        getAveragePeriodLength();
+    }
+
+    // get the pain data
+    function getPainData() {
+        // first empty old data
+        crampDays.length = 0;
+        noPainDays.length = 0;
+        backPainDays.length = 0;
+        headacheDays.length = 0;
+
+        // load in all pain data and make sure it is sorted by date
+        var painDates = new Map<string, string>(JSON.parse(localStorage.painMap));
+        const newMap = Array.from(painDates).sort((a, b) => moment(a[1]).diff(moment(b[1])));
+
+        const sortedPainDays = new Map(newMap);
+
+        // add each date to the corresponding pain type array
+        const today = moment();
+        sortedPainDays.forEach((pain: string, date: string) => {
+            const day = moment(date);
+            // only use tracking in the last month:
+            if (today.diff(day, 'days') < 31) {
+                if (!crampDays.includes(date) && pain == "Cramps") { crampDays.push(date); }
+                else if (!noPainDays.includes(date) && pain == "No Pain") { noPainDays.push(date); }
+                else if (!headacheDays.includes(date) && pain == "Headache") { headacheDays.push(date); }
+                else if (!backPainDays.includes(date) && pain == "Back Pain") { backPainDays.push(date); }
+            }
+            else {
+                // because the array is ordered, if one day is over 31 days away, they will all be so we can exit the function
+                return;
+            }
+
+        });
     }
 
 
@@ -148,41 +186,58 @@ const Analysis: React.FC = () => {
                 <IonGrid fixed={true}>
                     <IonRow><h2>Period Length</h2></IonRow>
                     <IonRow><BarChart
-                    dataset={periodLengths}
-                    xAxis={[{ scaleType: 'band', dataKey: 'startDate' }]}
-                    series={[
-                        { dataKey: 'length', label: 'Period Length' , color: 'var(--lighter-pink)'},
-                    ]}
-                    slotProps={{
-                        legend: {
-                          labelStyle: {
-                            fill: 'var(--text)',
-                          },
-                        },
-                      }}
-                    {...chartSetting}
+                        dataset={periodLengths}
+                        xAxis={[{ scaleType: 'band', dataKey: 'startDate', label: 'Start Date', }]}
+                        series={[
+                            { dataKey: 'length', label: 'Period Length', color: 'var(--lighter-pink)' },
+                        ]}
+                        slotProps={{
+                            legend: {
+                                labelStyle: {
+                                    fill: 'var(--text)',
+                                },
+                            },
+                        }}
+                        {...chartSetting}
                     /></IonRow>
                     <IonRow class="ion-justify-content-between"><p><b>Average Period Length: {averagePeriodLength} days</b></p> <IonButton>More Details</IonButton> </IonRow>
 
                     <IonRow><h2>Cycle Length</h2></IonRow>
                     <IonRow><BarChart
-                    dataset={cycleLengths}
-                    xAxis={[{ scaleType: 'band', dataKey: 'startDate' }]}
-                    series={[
-                        { dataKey: 'length', label: 'Cycle Length' , color: 'var(--lighter-pink)'},
-                    ]}
-                    slotProps={{
-                        legend: {
-                          labelStyle: {
-                            fill: 'var(--text)',
-                          },
-                        },
-                      }}
-                    {...chartSetting}
+                        dataset={cycleLengths}
+                        xAxis={[{ scaleType: 'band', dataKey: 'startDate' }]}
+                        series={[
+                            { dataKey: 'length', label: 'Cycle Length', color: 'var(--complementary-colour)' },
+                        ]}
+                        slotProps={{
+                            legend: {
+                                labelStyle: {
+                                    fill: 'var(--text)',
+                                },
+                            },
+                        }}
+                        {...chartSetting}
                     /></IonRow>
                     <IonRow class="ion-justify-content-between"><p><b>Average Cycle Length: {averageCycleLength} days</b></p> <IonButton>More Details</IonButton> </IonRow>
 
                     <IonRow><h2>Pain</h2></IonRow>
+                    <IonRow><p>This chart shows tracked pain data from the last 31 days:</p></IonRow>
+                    <IonRow><PieChart
+                    colors={['var(--mid-pink)', 'var(--complementary-colour)', 'var(--complementary-colour2)', 'var(--complementary-colour3)']}
+                        series={[
+                            {
+                                data: [
+                                    { id: 0, value: noPainDays.length, label: 'No Pain' },
+                                    { id: 1, value: crampDays.length, label: 'Cramps' },
+                                    { id: 2, value: backPainDays.length, label: 'Back Pain' },
+                                    { id: 3, value: headacheDays.length, label: 'Headache Pain' },
+                                ],
+                            },
+                        ]}
+                        width={500}
+                        height={200}
+                    /> </IonRow>
+
                 </IonGrid>
 
             </IonContent>
