06fcccdf9930b33724fc3d8607d1c9834c0ade06
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-10 12:00:26 +0000

    all tracking data is now stored

--- rosie/src/pages/Track.tsx
+++ rosie/src/pages/Track.tsx
@@ -15,10 +15,12 @@ const Track: React.FC = () => {
   const [clickedFlow, setClickedFlow] = useState<string>('');
   const [clickedPain, setClickedPain] = useState<string>('');
   const [clickedSkin, setClickedSkin] = useState<string>('');
+  const [clickedEmotion, setClickedEmotion] = useState<string>('');
 
   useEffect(() => {
     // when the page loads, set the selected date to today as default
     const today = moment();
+    setSelectedDate(today.format('YYYY-MM-DD'));
     setEvents([
       {
         title: '',
@@ -53,7 +55,7 @@ const Track: React.FC = () => {
     setClickedFlow(columnText);
   }
 
-  // handle a flow column being clicked
+  // handle a pain column being clicked
   function handlePainClick(event: any) {
     const columnText = event.currentTarget.innerText.trim(); // Get the column text
 
@@ -61,12 +63,87 @@ const Track: React.FC = () => {
     setClickedPain(columnText);
   }
 
-  // handle a flow column being clicked
+  // handle a skin column being clicked
   function handleSkinClick(event: any) {
     const columnText = event.currentTarget.innerText.trim(); // Get the column text
 
     // add the column to the clicked skin
     setClickedSkin(columnText);
+  }
+
+  // handle an emotion column being clicked
+  function handleEmotionClick(event: any) {
+    const columnText = event.currentTarget.innerText.trim(); // Get the column text
+
+    // add the column to the clicked emotion
+    setClickedEmotion(columnText);
+  }
+
+  /**
+   * Function to save all of the data that has been tracked to local storage
+   */
+  function saveTracking() {
+
+    // check if the pain map exists in local storage or if this is the first pain track
+    if (localStorage.getItem("painMap") === null) {
+      // create a map that maps a date to a pain level
+      let painMap = new Map<string, string>();
+      painMap.set(selectedDate, clickedPain);
+      localStorage.painMap = JSON.stringify(Array.from(painMap.entries()));
+    }
+    else {
+      // get the previous pain map, and add to it, overwriting if there is something for that date already
+      var storedPain = new Map(JSON.parse(localStorage.painMap));
+      storedPain.set(selectedDate, clickedPain);
+      localStorage.painMap = JSON.stringify(Array.from(storedPain.entries()));
+    }
+
+
+    // period flow data
+    // check if the period map exists in local storage or if this is the first period track
+    if (localStorage.getItem("periodMap") === null) {
+      // create a map that maps a date to a pain level
+      let periodMap = new Map<string, string>();
+      periodMap.set(selectedDate, clickedFlow);
+      localStorage.periodMap = JSON.stringify(Array.from(periodMap.entries()));
+    }
+    else {
+      // get the previous pain map, and add to it, overwriting if there is something for that date already
+      var storedPeriod = new Map(JSON.parse(localStorage.periodMap));
+      storedPeriod.set(selectedDate, clickedFlow);
+      localStorage.periodMap = JSON.stringify(Array.from(storedPeriod.entries()));
+    }
+
+    // emotions data
+    // check if the emotions map exists in local storage or if this is the first
+    if (localStorage.getItem("emotionsMap") === null) {
+      // create a map that maps a date to a pain level
+      let emotionsMap = new Map<string, string>();
+      emotionsMap.set(selectedDate, clickedEmotion);
+      localStorage.emotionsMap = JSON.stringify(Array.from(emotionsMap.entries()));
+    }
+    else {
+      // get the previous pain map, and add to it, overwriting if there is something for that date already
+      var storedEmotions= new Map(JSON.parse(localStorage.emotionsMap));
+      storedEmotions.set(selectedDate, clickedEmotion);
+      localStorage.emotionsMap = JSON.stringify(Array.from(storedEmotions.entries()));
+    }
+
+    // skin data
+    // check if the skin map exists in local storage or if this is the first
+    if (localStorage.getItem("skinMap") === null) {
+      // create a map that maps a date to a pain level
+      let skinMap = new Map<string, string>();
+      skinMap.set(selectedDate, clickedSkin);
+      localStorage.skinMap = JSON.stringify(Array.from(skinMap.entries()));
+    }
+    else {
+      // get the previous pain map, and add to it, overwriting if there is something for that date already
+      var storedSkin= new Map(JSON.parse(localStorage.skinMap));
+      storedSkin.set(selectedDate, clickedSkin);
+      localStorage.skinMap = JSON.stringify(Array.from(storedSkin.entries()));
+    }
+    
   }
 
 
@@ -117,91 +194,91 @@ const Track: React.FC = () => {
               // clicked css class is added when the column is clicked
               className={`ion-activatable ripple-parent ${clickedFlow.includes('Medium') ? 'clicked clickedIcon' : ''}`}
               onClick={handleFlowClick}>
-                <IonIcon icon={water}  className='colourIcon'></IonIcon><br></br>Medium<IonRippleEffect className="custom-ripple " /></IonCol>
+              <IonIcon icon={water} className='colourIcon'></IonIcon><br></br>Medium<IonRippleEffect className="custom-ripple " /></IonCol>
             <IonCol
               // clicked css class is added when the column is clicked
               className={`ion-activatable ripple-parent ${clickedFlow.includes('Light') ? 'clicked clickedIcon' : ''}`}
               onClick={handleFlowClick}>
-                <IonIcon icon={water}  className='colourIcon'></IonIcon><br></br>Light<IonRippleEffect className="custom-ripple" /></IonCol>
+              <IonIcon icon={water} className='colourIcon'></IonIcon><br></br>Light<IonRippleEffect className="custom-ripple" /></IonCol>
           </IonRow>
           <IonRow class="ion-justify-content-start">
             <h2>Pain</h2>
           </IonRow>
           <IonRow className="track-page trackRows">
-          <IonCol
+            <IonCol
               // clicked css class is added when the column is clicked
               className={`ion-activatable ripple-parent ${clickedPain.includes('No Pain') ? 'clicked clickedIcon' : ''}`}
               onClick={handlePainClick}>
-                <IonIcon icon={happyOutline} className='colourIcon'></IonIcon><br></br>No Pain<IonRippleEffect className="custom-ripple" /></IonCol>
+              <IonIcon icon={happyOutline} className='colourIcon'></IonIcon><br></br>No Pain<IonRippleEffect className="custom-ripple" /></IonCol>
             <IonCol
               // clicked css class is added when the column is clicked
               className={`ion-activatable ripple-parent ${clickedPain.includes('Cramps') ? 'clicked clickedIcon' : ''}`}
               onClick={handlePainClick}>
-                <IonIcon icon={flash} className='colourIcon'></IonIcon><br></br>Cramps<IonRippleEffect className="custom-ripple" /></IonCol>
+              <IonIcon icon={flash} className='colourIcon'></IonIcon><br></br>Cramps<IonRippleEffect className="custom-ripple" /></IonCol>
             <IonCol
               // clicked css class is added when the column is clicked
               className={`ion-activatable ripple-parent ${clickedPain.includes('Back Pain') ? 'clicked clickedIcon' : ''}`}
               onClick={handlePainClick}>
-                <IonIcon icon={man} className='colourIcon'></IonIcon><br></br>Back Pain<IonRippleEffect className="custom-ripple" /></IonCol>
+              <IonIcon icon={man} className='colourIcon'></IonIcon><br></br>Back Pain<IonRippleEffect className="custom-ripple" /></IonCol>
             <IonCol
               // clicked css class is added when the column is clicked
               className={`ion-activatable ripple-parent ${clickedPain.includes('Headache') ? 'clicked clickedIcon' : ''}`}
               onClick={handlePainClick}>
-                <IonIcon icon={sadOutline} className='colourIcon'></IonIcon><br></br>Headache<IonRippleEffect className="custom-ripple" /></IonCol>
+              <IonIcon icon={sadOutline} className='colourIcon'></IonIcon><br></br>Headache<IonRippleEffect className="custom-ripple" /></IonCol>
           </IonRow>
           <IonRow class="ion-justify-content-between">
             <IonCol size="3"><h2>Skin</h2></IonCol>
             <IonCol size="3"><IonButton size='small'>Add Photo <IonIcon icon={add} className='buttonIcon'></IonIcon></IonButton></IonCol>
           </IonRow>
           <IonRow className="track-page trackRows">
-          <IonCol
+            <IonCol
               // clicked css class is added when the column is clicked
               className={`ion-activatable ripple-parent ${clickedSkin.includes('Good') ? 'clicked clickedIcon' : ''}`}
               onClick={handleSkinClick}>
-                <IonIcon icon={happyOutline} className='colourIcon'></IonIcon><br></br>Good<IonRippleEffect className="custom-ripple" /></IonCol>
+              <IonIcon icon={happyOutline} className='colourIcon'></IonIcon><br></br>Good<IonRippleEffect className="custom-ripple" /></IonCol>
             <IonCol
               // clicked css class is added when the column is clicked
               className={`ion-activatable ripple-parent ${clickedSkin.includes('Acne') ? 'clicked clickedIcon' : ''}`}
               onClick={handleSkinClick}>
-                <IonIcon icon={ellipsisHorizontal} className='colourIcon'></IonIcon><br></br>Acne<IonRippleEffect className="custom-ripple" /></IonCol>
+              <IonIcon icon={ellipsisHorizontal} className='colourIcon'></IonIcon><br></br>Acne<IonRippleEffect className="custom-ripple" /></IonCol>
             <IonCol
               // clicked css class is added when the column is clicked
               className={`ion-activatable ripple-parent ${clickedSkin.includes('Oily') ? 'clicked clickedIcon' : ''}`}
               onClick={handleSkinClick}>
-                <IonIcon icon={water} className='colourIcon'></IonIcon><br></br>Oily<IonRippleEffect className="custom-ripple" /></IonCol>
+              <IonIcon icon={water} className='colourIcon'></IonIcon><br></br>Oily<IonRippleEffect className="custom-ripple" /></IonCol>
             <IonCol
               // clicked css class is added when the column is clicked
               className={`ion-activatable ripple-parent ${clickedSkin.includes('Dry') ? 'clicked clickedIcon' : ''}`}
               onClick={handleSkinClick}>
-                <IonIcon icon={bandage} className='colourIcon'></IonIcon><br></br>Dry<IonRippleEffect className="custom-ripple" /></IonCol>
+              <IonIcon icon={bandage} className='colourIcon'></IonIcon><br></br>Dry<IonRippleEffect className="custom-ripple" /></IonCol>
           </IonRow>
           <IonRow class="ion-justify-content-start">
             <h2>Emotions</h2>
           </IonRow>
           <IonRow className="track-page trackRows">
-          <IonCol
-              // clicked css class is added when the column is clicked
-              className={`ion-activatable ripple-parent ${clickedSkin.includes('Good') ? 'clicked clickedIcon' : ''}`}
-              onClick={handleSkinClick}>
-                <IonIcon icon={happyOutline} className='colourIcon'></IonIcon><br></br>Happy<IonRippleEffect className="custom-ripple" /></IonCol>
             <IonCol
               // clicked css class is added when the column is clicked
-              className={`ion-activatable ripple-parent ${clickedSkin.includes('Acne') ? 'clicked clickedIcon' : ''}`}
-              onClick={handleSkinClick}>
-                <IonIcon icon={sadOutline} className='colourIcon'></IonIcon><br></br>Sad<IonRippleEffect className="custom-ripple" /></IonCol>
+              className={`ion-activatable ripple-parent ${clickedEmotion.includes('Happy') ? 'clicked clickedIcon' : ''}`}
+              onClick={handleEmotionClick}>
+              <IonIcon icon={happyOutline} className='colourIcon'></IonIcon><br></br>Happy<IonRippleEffect className="custom-ripple" /></IonCol>
             <IonCol
               // clicked css class is added when the column is clicked
-              className={`ion-activatable ripple-parent ${clickedSkin.includes('Oily') ? 'clicked clickedIcon' : ''}`}
-              onClick={handleSkinClick}>
-                <IonIcon icon={thunderstorm} className='colourIcon'></IonIcon><br></br>Angry<IonRippleEffect className="custom-ripple" /></IonCol>
+              className={`ion-activatable ripple-parent ${clickedEmotion.includes('Sad') ? 'clicked clickedIcon' : ''}`}
+              onClick={handleEmotionClick}>
+              <IonIcon icon={sadOutline} className='colourIcon'></IonIcon><br></br>Sad<IonRippleEffect className="custom-ripple" /></IonCol>
             <IonCol
               // clicked css class is added when the column is clicked
-              className={`ion-activatable ripple-parent ${clickedSkin.includes('Dry') ? 'clicked clickedIcon' : ''}`}
-              onClick={handleSkinClick}>
-                <IonIcon icon={pulse} className='colourIcon'></IonIcon><br></br>Mood Swings<IonRippleEffect className="custom-ripple" /></IonCol>
+              className={`ion-activatable ripple-parent ${clickedEmotion.includes('Angry') ? 'clicked clickedIcon' : ''}`}
+              onClick={handleEmotionClick}>
+              <IonIcon icon={thunderstorm} className='colourIcon'></IonIcon><br></br>Angry<IonRippleEffect className="custom-ripple" /></IonCol>
+            <IonCol
+              // clicked css class is added when the column is clicked
+              className={`ion-activatable ripple-parent ${clickedEmotion.includes('Mood Swings') ? 'clicked clickedIcon' : ''}`}
+              onClick={handleEmotionClick}>
+              <IonIcon icon={pulse} className='colourIcon'></IonIcon><br></br>Mood Swings<IonRippleEffect className="custom-ripple" /></IonCol>
           </IonRow>
           <IonRow class="ion-justify-content-center">
-            <IonButton className="btn" size="large">Save</IonButton>
+            <IonButton className="btn" onClick={saveTracking} size="large">Save</IonButton>
           </IonRow>
         </IonGrid>
       </IonContent>
