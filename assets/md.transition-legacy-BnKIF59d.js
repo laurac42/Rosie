d0600844445e72534bf44e640777018c095d8157
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-11 22:26:14 +0000

    Updates

--- assets/md.transition-legacy-BnKIF59d.js
+++ assets/md.transition-legacy-BnKIF59d.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CRh-MlbF.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-BhGOfC4m.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
