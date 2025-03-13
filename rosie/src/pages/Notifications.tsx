e325ef78080ba3d0f08c491b614b9e994995252b
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-13 18:13:34 +0000

    updated button path

--- rosie/src/pages/Notifications.tsx
+++ rosie/src/pages/Notifications.tsx
@@ -55,14 +55,13 @@ const Notifications: React.FC = () => {
                 </IonHeader>
                 <IonContent fullscreen>
                 <IonGrid fixed={true}>
-
                     <IonRow class="ion-justify-content-start">
-                        <p><b>Notifications:</b></p>
+                        <IonRow class="ion-justify-content-center"><h1 className="heading"><IonIcon icon={flower} className='colourIcon'></IonIcon> Choose Your Notifications <IonIcon icon={flower} className='colourIcon'></IonIcon></h1></IonRow>
                     </IonRow>
                     <IonRow class="checkbox"><IonCheckbox labelPlacement="end">Upcoming Period Reminder</IonCheckbox></IonRow>
                     <IonRow><IonCheckbox labelPlacement="end">Daily Track Reminder</IonCheckbox></IonRow>
                     <IonRow class="ion-justify-content-center">
-                        <IonButton className="btn" href="/Rosie/SignUp/PrivacyPolicy" size="large">Save Preferences</IonButton>
+                        <IonButton className="btn" href="/Rosie/Cycle" size="large">Save Notifications</IonButton>
                     </IonRow>
                 </IonGrid>
             </IonContent>
