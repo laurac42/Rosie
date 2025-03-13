46f0839d38e6ce95e3a46e612131667a85627956
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-13 19:30:00 +0000

    upated menu options

--- rosie/src/pages/Track.tsx
+++ rosie/src/pages/Track.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonList, IonMenu, IonButtons, IonMenuButton, IonButton, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/react';
-import { add, bandage, bluetooth, colorPalette, ellipsisHorizontal, flash, flower, happy, happyOutline, informationCircle, lockClosed, man, notifications, people, personCircle, pulse, sadOutline, thunderstorm, water, waterOutline, wifi } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonAccordionGroup, IonAccordion, IonItem, IonLabel, IonList, IonMenu, IonButtons, IonMenuButton, IonButton, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { add, bandage, bluetooth, calendar, clipboard, colorPalette, ellipsisHorizontal, flash, flower, folderOpen, happy, happyOutline, informationCircle, lockClosed, man, notifications, people, personCircle, pulse, radioButtonOff, sadOutline, settings, thunderstorm, trendingUp, water, waterOutline, wifi } from 'ionicons/icons';
 import { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -180,30 +180,77 @@ const Track: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonContent>
-        <IonList>
-        <IonItem href="/Rosie/Menu/Appearance">
-          <IonIcon className="menuIcon" aria-hidden="true" icon={colorPalette} slot="start"></IonIcon>
-          <IonLabel>Appearance</IonLabel>
-        </IonItem>
-        <IonItem href='/Rosie/Menu/Notifications'>
-          <IonIcon className="menuIcon" aria-hidden="true" icon={notifications} slot="start"></IonIcon>
-          <IonLabel>Notifications</IonLabel>
-        </IonItem>
-        <IonItem href="/Rosie/Menu/AboutUs">
-          <IonIcon className="menuIcon" aria-hidden="true" icon={people} slot="start"></IonIcon>
-          <IonLabel>About Us</IonLabel>
-        </IonItem>
-        <IonItem href='/Rosie/Menu/PrivacyPolicy'>
-          <IonIcon className="menuIcon" aria-hidden="true" icon={lockClosed} slot="start"></IonIcon>
-          <IonLabel>Privacy Policy</IonLabel>
-        </IonItem>
-        <IonItem href='/Rosie/Menu/Resources'>
-          <IonIcon className="menuIcon" aria-hidden="true" icon={informationCircle} slot="start"></IonIcon>
-          <IonLabel>Resources</IonLabel>
-        </IonItem>
-      </IonList>
+
+          <IonList>
+            <IonAccordionGroup>
+              <IonAccordion value="first">
+                <IonItem slot="header">
+                  <IonIcon className="menuIcon" aria-hidden="true" icon={folderOpen} slot="start"></IonIcon>
+                  <IonLabel>Main Pages</IonLabel>
+                </IonItem>
+                <div className="ion-padding" slot="content">
+                  <IonItem href="/Rosie/Cycle">
+                    <IonIcon className="menuIcon" aria-hidden="true" icon={radioButtonOff} slot="start"></IonIcon>
+                    <IonLabel>Cycle</IonLabel>
+                  </IonItem>
+                </div>
+                <div className="ion-padding" slot="content">
+                  <IonItem href="/Rosie/Calendar">
+                    <IonIcon className="menuIcon" aria-hidden="true" icon={calendar} slot="start"></IonIcon>
+                    <IonLabel>Calendar</IonLabel>
+                  </IonItem>
+                </div>
+                <div className="ion-padding" slot="content">
+                  <IonItem href="/Rosie/Track">
+                    <IonIcon className="menuIcon" aria-hidden="true" icon={clipboard} slot="start"></IonIcon>
+                    <IonLabel>Cycle</IonLabel>
+                  </IonItem>
+                </div>
+                <div className="ion-padding" slot="content">
+                  <IonItem href="/Rosie/Analysis">
+                    <IonIcon className="menuIcon" aria-hidden="true" icon={trendingUp} slot="start"></IonIcon>
+                    <IonLabel>Cycle</IonLabel>
+                  </IonItem>
+                </div>
+              </IonAccordion>
+            
+
+            <IonAccordion value="second">
+              <IonItem slot="header">
+                <IonIcon className="menuIcon" aria-hidden="true" icon={settings} slot="start"></IonIcon>
+                <IonLabel>Settings</IonLabel>
+              </IonItem>
+              <div className="ion-padding" slot="content">
+                <IonItem href="/Rosie/Menu/Appearance">
+                  <IonIcon className="menuIcon" aria-hidden="true" icon={colorPalette} slot="start"></IonIcon>
+                  <IonLabel>Appearance</IonLabel>
+                </IonItem>
+              </div>
+              <div className="ion-padding" slot="content">
+                <IonItem href='/Rosie/Menu/Notifications'>
+                  <IonIcon className="menuIcon" aria-hidden="true" icon={notifications} slot="start"></IonIcon>
+                  <IonLabel>Notifications</IonLabel>
+                </IonItem>
+              </div>
+            </IonAccordion>
+            </IonAccordionGroup>
+
+            <IonItem href="/Rosie/Menu/AboutUs">
+              <IonIcon className="menuIcon" aria-hidden="true" icon={people} slot="start"></IonIcon>
+              <IonLabel>About Us</IonLabel>
+            </IonItem>
+            <IonItem href='/Rosie/Menu/PrivacyPolicy'>
+              <IonIcon className="menuIcon" aria-hidden="true" icon={lockClosed} slot="start"></IonIcon>
+              <IonLabel>Privacy Policy</IonLabel>
+            </IonItem>
+            <IonItem href='/Rosie/Menu/Resources'>
+              <IonIcon className="menuIcon" aria-hidden="true" icon={informationCircle} slot="start"></IonIcon>
+              <IonLabel>Resources</IonLabel>
+            </IonItem>
+
+          </IonList>
         </IonContent>
-        
+
       </IonMenu>
       <IonPage id="main-content">
         <IonHeader>
