dfd9da64a67df51941a4216e768aabf6b2b6ad3e
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-13 18:00:00 +0000

    styling notifications settings better

--- rosie/src/pages/Preferences.tsx
+++ rosie/src/pages/Preferences.tsx
@@ -13,7 +13,7 @@ const Preferences: React.FC = () => {
     const handleThemeChange = (e: CustomEvent<any>) => {
         setTheme(e.detail.value);
         applyTheme(e.detail.value);
-      };
+    };
 
     return (
         <IonPage>
@@ -27,19 +27,22 @@ const Preferences: React.FC = () => {
                     <IonRow class="ion-justify-content-center"><h1 className="heading"><IonIcon icon={flower} className='colourIcon'></IonIcon> Choose Preferences <IonIcon icon={flower} className='colourIcon'></IonIcon></h1></IonRow>
                     <IonRow class="ion-justify-content-start"><p><b> Appearance:</b> </p></IonRow>
                     <IonRadioGroup value={theme} onIonChange={handleThemeChange}>
-                        <IonRow class="ion-justify-content-start"><IonRadio  value="light" labelPlacement="end" justify="space-between">Light Mode</IonRadio></IonRow>
+                        <IonRow class="ion-justify-content-start"><IonRadio value="light" labelPlacement="end" justify="space-between">Light Mode</IonRadio></IonRow>
                         <br />
                         <IonRow class="ion-justify-content-start"><IonRadio value="dark" justify="space-between" labelPlacement='end' >Dark Mode</IonRadio></IonRow>
                         <br />
                         <IonRow class="ion-justify-content-start"><IonRadio value="contrast" labelPlacement="end" justify="space-between">High Contrast Mode</IonRadio></IonRow>
                         <br />
                     </IonRadioGroup>
-                   
+
                     <IonRow class="ion-justify-content-start">
                         <p><b>Notifications:</b></p>
                     </IonRow>
-                    <IonCheckbox labelPlacement="end">Upcoming Period Reminder</IonCheckbox>
-                    <IonCheckbox labelPlacement="end">Daily Track Reminder</IonCheckbox>
+                    <IonRow class="checkbox"><IonCheckbox labelPlacement="end">Upcoming Period Reminder</IonCheckbox></IonRow>
+                    <IonRow><IonCheckbox labelPlacement="end">Daily Track Reminder</IonCheckbox></IonRow>
+                    <IonRow class="ion-justify-content-center">
+                        <IonButton className="btn" href="/Rosie/SignUp/PrivacyPolicy" size="large">Save Preferences</IonButton>
+                    </IonRow>
                     <IonRow class="ion-justify-content-center">
                         <IonButton className="btn" href="/Rosie/SignUp/PrivacyPolicy" size="large">Save Preferences</IonButton>
                     </IonRow>
