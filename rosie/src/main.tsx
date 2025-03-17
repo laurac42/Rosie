4aa233f89c0757dc822aa18a159f88415ab2e41e
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-17 21:36:06 +0000

    daily notification when on the app

--- rosie/src/main.tsx
+++ rosie/src/main.tsx
@@ -12,3 +12,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// register service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/Rosie/service-worker.js')
+  .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+  }).catch(function(error) {
+      console.log('Service Worker registration failed:', error);
+  });
+}
--- rosie/src/pages/Preferences.tsx
+++ rosie/src/pages/Preferences.tsx
@@ -7,6 +7,7 @@ import ExploreContainer from '../components/ExploreContainer';
 
 const Preferences: React.FC = () => {
     const [theme, setTheme] = useState<string>('');
+    const [notifications, setNotifications] = useState<string[]>([]);
 
     // to handle the selection of a theme change radio button
     // sets the theme constant to whatever was the theme selected by the radio buttons
@@ -14,6 +15,42 @@ const Preferences: React.FC = () => {
         setTheme(e.detail.value);
         applyTheme(e.detail.value);
     };
+
+    function requestPermission() {
+        Notification.requestPermission().then((result) => {
+            if (result === "granted") {
+                console.log("saving to local storage");
+                randomNotification();
+            }
+            localStorage.setItem('Notifications', result); // set the local storage to granted so it can be accessed everywhere
+        })
+    }    
+
+    function clickedNotification(e: CustomEvent<any>) {
+        console.log(e.detail.value);
+        // if the notifications array includes what was clicked already, it needs to be removed
+        // if it does not include it, it needs to be added
+        if (notifications.includes(e.detail.value)) {
+            var index = notifications.indexOf(e.detail.value);
+            notifications.splice(index, 1);
+        }
+        else {
+            notifications.push(e.detail.value);
+        }
+        localStorage.chosenNotifications = JSON.stringify(notifications);
+    }
+
+    function randomNotification() {
+        const notifTitle = "This is a notification";
+        const notifBody = `Created by Laura`;
+        const notifImg = `/Rosie/rose.png`;
+        const options = {
+          body: notifBody,
+          icon: notifImg,
+        };
+        new Notification(notifTitle, options);
+        //setTimeout(randomNotification, 30000);
+      }
 
     return (
         <IonPage>
@@ -39,10 +76,10 @@ const Preferences: React.FC = () => {
                     <IonRow class="ion-justify-content-start">
                         <p><b>Notifications:</b></p>
                     </IonRow>
-                    <IonRow class="checkbox"><IonCheckbox labelPlacement="end">Upcoming Period Reminder</IonCheckbox></IonRow>
-                    <IonRow><IonCheckbox labelPlacement="end">Daily Track Reminder</IonCheckbox></IonRow>
+                    <IonRow class="checkbox"><IonCheckbox onIonChange={clickedNotification} value="upcoming" labelPlacement="end">Upcoming Period Reminder</IonCheckbox></IonRow>
+                    <IonRow><IonCheckbox onIonChange={clickedNotification} value="daily" labelPlacement="end">Daily Track Reminder</IonCheckbox></IonRow>
                     <IonRow class="ion-justify-content-center">
-                        <IonButton className="btn" href="/Rosie/SignUp/PrivacyPolicy" size="large">Save Preferences</IonButton>
+                        <IonButton onClick={requestPermission} className="btn" size="large">Save Preferences</IonButton>
                     </IonRow>
                 </IonGrid>
             </IonContent>
