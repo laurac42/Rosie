f4b11262c516e1dd15670c6dac37f40212375b51
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-20 21:42:58 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,14 @@
 // this means that the service worker is actively listening for push events. So if it hears on in the background it will respond
+// came from - https://github.com/mdn/serviceworker-cookbook/blob/master/push-payload/service-worker.js
 self.addEventListener('push', function(event) {
     // Retrieve the textual payload from event.data (a PushMessageData object).
     // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
     // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
-    const payload = event.data ? event.data.text() : 'no payload';
+    const payload = event.data ? event.data.text() : 'Reminder from Rosie';
   
     // Keep the service worker alive until the notification is created.
     event.waitUntil(
-      // Show a notification with title 'ServiceWorker Cookbook' and use the payload
-      // as the body.
-      self.registration.showNotification('Period Tracking Notification', {
+      self.registration.showNotification('Track your Period', {
         body: payload,
         icon: "/Rosie/rose.png"
       })
