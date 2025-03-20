84431df33e27119ed1b9b26c3a1d403a5998fd04
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-20 23:30:33 +0000

    Updates

--- assets/ios.transition-legacy-C_GuL5SD.js
+++ assets/ios.transition-legacy-C_GuL5SD.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CK1q7Hyv.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
+System.register(["./index-legacy-CWO-sJB4.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
