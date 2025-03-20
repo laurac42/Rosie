64392ee95110d383aeb6ba2a0ec3ea4548722440
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-20 10:06:38 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,17 @@
-var CACHE_NAME = 'Rosie-localStorage';
-var urlsToCache = [
-  './',
-  './Rosie/'
-];
-
-self.addEventListener('install', function(event) {
-    console.log("service worked installed")
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-    console.log("fetching")
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
+// this means that the service worker is actively listening for push events. So if it hears on in the background it will respond
+self.addEventListener('push', function(event) {
+    // Retrieve the textual payload from event.data (a PushMessageData object).
+    // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
+    // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
+    const payload = event.data ? event.data.text() : 'no payload';
+  
+    // Keep the service worker alive until the notification is created.
+    event.waitUntil(
+      // Show a notification with title 'ServiceWorker Cookbook' and use the payload
+      // as the body.
+      self.registration.showNotification('Period Tracking Notification', {
+        body: payload,
+        icon: "/Rosie/rose.png"
       })
     );
-});
-
-// Front-end **Service Worker** listening for events
-self.addEventListener("push", (event) => {
-    const data = event.data.json() // Payload from the server, assumed to be in JSON format
-  
-    event.waitUntil(
-      // Upon receiving the push event, call **Notifications API** to push the notification
-      self.registration.showNotification(
-        data.title ? data.title : "New Message",
-        {
-          body: data.body,
-          badge: "rose.png",
-          vibrate: [200, 100, 200],
-          timestamp: Date.now(),
-          data: { use_to_open_specific_page: data.props }, // Custom data sent from the server
-        }
-      )
-    )
-})
+  });
