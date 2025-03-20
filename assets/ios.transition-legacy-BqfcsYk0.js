a57e15560ee6d90f91b2f088af7778fbbb256be9
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-20 22:04:11 +0000

    Updates

--- assets/ios.transition-legacy-BqfcsYk0.js
+++ assets/ios.transition-legacy-BqfcsYk0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DszdXhUl.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
+System.register(["./index-legacy-DebX8RTr.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
