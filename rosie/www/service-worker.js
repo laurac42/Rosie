0187e403fb0aad971be188203eeb46950e3b970a
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-20 23:25:42 +0000

    deployment

--- rosie/www/service-worker.js
+++ rosie/www/service-worker.js
@@ -1,17 +1,62 @@
 // this means that the service worker is actively listening for push events. So if it hears on in the background it will respond
+// came from - https://github.com/mdn/serviceworker-cookbook/blob/master/push-payload/service-worker.js
 self.addEventListener('push', function(event) {
     // Retrieve the textual payload from event.data (a PushMessageData object).
     // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
     // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
-    const payload = event.data ? event.data.text() : 'no payload';
+    const payload = event.data ? event.data.text() : 'Reminder from Rosie';
   
     // Keep the service worker alive until the notification is created.
     event.waitUntil(
-      // Show a notification with title 'ServiceWorker Cookbook' and use the payload
-      // as the body.
-      self.registration.showNotification('Period Tracking Notification', {
+      self.registration.showNotification('Track your Period', {
         body: payload,
         icon: "/Rosie/rose.png"
       })
     );
   });
+
+  var CACHE_NAME = 'rosie-PWA-localStorage';
+var urlsToCache = [
+  './Rosie/SignUp',
+  './Rosie/SignUp/Preferences'
+];
+
+// on install
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+    );
+});
