93a646fafd12398e6296442c3fce0915ad6c96ed
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-20 23:16:24 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,8 @@ self.addEventListener('push', function(event) {
 
   var CACHE_NAME = 'rosie-PWA-localStorage';
 var urlsToCache = [
-  './'
+  './Rosie/SignUp',
+  './Rosie/SignUp/Preferences'
 ];
 
 // on install
