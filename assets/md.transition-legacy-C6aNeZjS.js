7a4d658ee0a0290f0d0e3af504e0916910ecbf85
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-20 23:42:26 +0000

    Updates

--- assets/md.transition-legacy-C6aNeZjS.js
+++ assets/md.transition-legacy-C6aNeZjS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CWO-sJB4.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
+System.register(["./index-legacy-BsExvWfA.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
