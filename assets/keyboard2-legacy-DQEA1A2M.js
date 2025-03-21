59be00638450ee503077c170f89e6dc0004285c7
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-21 11:22:04 +0000

    Updates

--- assets/keyboard2-legacy-DQEA1A2M.js
+++ assets/keyboard2-legacy-DQEA1A2M.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BsExvWfA.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-D_LQvNIa.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
