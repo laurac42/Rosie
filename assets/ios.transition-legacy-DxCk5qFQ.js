2825375c571bea55743fed47217b9bec3b3614ec
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-22 18:37:59 +0000

    Updates

--- assets/ios.transition-legacy-DxCk5qFQ.js
+++ assets/ios.transition-legacy-DxCk5qFQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CgvmbeJI.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
+System.register(["./index-legacy-KriQie8f.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-jNLn-CFX.js
+++ assets/md.transition-legacy-jNLn-CFX.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CgvmbeJI.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
+System.register(["./index-legacy-KriQie8f.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.v}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
