90eeec7b985d8fbe974ed9e1befc13da33b35ffd
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-03-22 22:42:23 +0000

    Updates

--- assets/swipe-back-legacy-BfPhA8cI.js
+++ assets/swipe-back-legacy-BfPhA8cI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CfXTICyM.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-QZpRNKv4.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
